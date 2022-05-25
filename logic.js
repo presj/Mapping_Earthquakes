20565e8f28def88003ec791eacac60b21c4ef0c0
presj <100803302+presj@users.noreply.github.com>
2022-05-24 22:45:25 -0400

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,31 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Assign variable map to the object L.map and instantiate the object with string mapid
+// mapid references the id tag in the div element on the html file
+// Create the map object with a center and zoom level (scale of 0-18).
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+// alternative is let map = L.map("mapid", { center: [40.7, -94.5], zoom: 4}); Used when adding multiple tile layers or background image of map
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city){
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
+
+// Assign the tile layer method that will be the background of our map from tile layer API's
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
